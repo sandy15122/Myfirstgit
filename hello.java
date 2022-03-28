e6c8e7c57d1cc93321e2bf899b90fb0b80668f0e
sandeep <sandeep.harishetty@gmail.com>
2022-03-28 04:44:59 +0000

    added the dwouble slash to commect

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Bindu, World!"); 
+        System.out.println("Hello Bindu, World!");
+       // We have added the below line	
 	System.out.println("hello Sandeep");
     }
 }
