ec77a0f72e5cc64cfcbb5c54eec865d2fb7ede6a
sandeep <sandeep.harishetty@gmail.com>
2022-03-28 08:00:27 +0000

    we have restored the file

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Bindu, World!");
-       // We have added the below line	
-	System.out.println("hello Sandeep");
+        System.out.println("Hello Bindu, World!"); 
     }
 }
